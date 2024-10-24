35940deaff65847d6163cd4085ff6baf5b7f7143
goblincookies <goblincookiebox@gmail.com>
2024-10-24 14:23:17 -0400

    completes javascript exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
+const sumAll = function( valA, valB) {
 
+    // VERIFY BOTH ARE INTEGERS
+    if ( !Number.isInteger(valA) | !Number.isInteger(valB) ) {
+        return "ERROR";
+    }
+    // VERIFY BOTH ARE GREATER THAN 0
+    if (valA < 0 | valB < 0 ) {
+        return "ERROR";
+    }
+    let sum = 0;
+    // sum
+    for (let i = Math.min(valA, valB); i <= Math.max(valA, valB); i++ ) {
+        sum += i;
+    }
+
+    // return
+    return sum;
 };
+
+console.log( sumAll(1,5));
 
 // Do not edit below this line
 module.exports = sumAll;
