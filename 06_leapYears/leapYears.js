a007409d73b9dd74728a967b0ce4f5f731b5f9b4
goblincookies <goblincookiebox@gmail.com>
2024-10-24 14:47:16 -0400

    completes javascript exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,12 @@
-const leapYears = function() {
+const leapYears = function( year ) {
 
+    // CHECK 4 NOT 100 || 400
+    if (( year%4==0 && year%100 != 0) || (year % 400==0) ){
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
 module.exports = leapYears;
+
