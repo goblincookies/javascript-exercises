1b4e7b5dfb0e9c41505f66d986fdea29111c794f
goblincookies <goblincookiebox@gmail.com>
2024-10-24 13:19:52 -0400

    completes javascript exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function( val ) {
+    // SPLIT STRING INTO ARRAY
+    let valRev = val.split("");
+    // REVERSE ARRAY
+    valRev = valRev.reverse();
+    // REJOIN ARRAY
+    valRev = valRev.join("");
 
+    return valRev;
 };
 
 // Do not edit below this line
