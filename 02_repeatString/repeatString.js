7c6baa6851124068458098bc4cbc197b82b63e1f
goblincookies <goblincookiebox@gmail.com>
2024-10-24 13:07:49 -0400

    completes javascript exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function( val, count ) {
+    if (count < 0) {
+        return "ERROR";
+    }
+    let ret_val = ""
+    for (let i = 0; i < count; i++) {
+        ret_val += val;
+    }
+    return ret_val;
 };
+
+console.log(repeatString('hey',3));
 
 // Do not edit below this line
 module.exports = repeatString;
