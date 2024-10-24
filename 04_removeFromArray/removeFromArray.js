6d1d91239305bd343dcd941860e8503c50db9923
goblincookies <goblincookiebox@gmail.com>
2024-10-24 13:57:10 -0400

    completes javascript exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function( arr, ...args) {
+    let cleanArray = [];
+    
+    // REBUILDS THE ENTIRE ARRAY IN cleanArray
+    // OMMITING ELEMENTS FOUND IN  args 
+    for (let i =0; i < arr.length; i++ ) {
+        if (!args.includes( arr[i]) ) {
+            cleanArray.push(arr[i]);
+        }
+    }
+    
+    return cleanArray;
 };
 
+// console.log( removeFromArray([0,1,2], 2,3 ));
 // Do not edit below this line
 module.exports = removeFromArray;
