f3c61528b7b336d0714a1c4637ce9bdcf3c1cf6e
goblincookies <goblincookiebox@gmail.com>
2024-10-25 12:41:55 -0400

    completes javascript exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,26 @@
-const convertToCelsius = function() {
+const convertToCelsius = function( val ) {
+  // (32°F − 32) × 5/9 = 0°C
+  // ROUND TO ONE DECIMAL
+  // val is temp in Fahrenheit
+
+  let cel = (val - 32) * (5/9);
+  cel = Math.round(cel*10)/10;
+  return cel;
 };
 
-const convertToFahrenheit = function() {
+
+const convertToFahrenheit = function( val ) {
+  // (0°C × 9/5) + 32 = 32°F
+  // ROUND TO ONE DECIMAL
+  // val is temp in Celcius
+
+  let fahr = (val * (9/5)) + 32;
+  fahr = Math.round(fahr*10)/10;
+  return fahr;
 };
+
+console.log(convertToCelsius(-97));
+console.log(convertToFahrenheit(-97));
 
 // Do not edit below this line
 module.exports = {
