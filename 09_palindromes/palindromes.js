5d006886035e1178614015aa5d3d88ee8b2ad28c
goblincookies <goblincookiebox@gmail.com>
2024-11-08 13:16:41 -0500

    completes javascript exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
+const palindromes = function ( val ) {
+    // STRIP PUNCTUALTION
+    let a = val.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+    // STRIP SPACES
+    let b = a.replace(/\s{1,}/g,"");
+    // CLEAN CAPITALS
+    b = b.toLowerCase()
 
+    for (let i=0; i<Math.floor(b.length/2); i++) {
+        if ( b[i] != b[ b.length-(1+i) ] ) {
+            return false
+        }
+    }
+    return true;
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
