d419eb75847924a22cf0882952ac0ff4e88f36eb
goblincookies <goblincookiebox@gmail.com>
2024-11-08 14:29:18 -0500

    completes javascript excercise 10 & 11

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
 
+const fibonacci = function( val ) {
+    val = parseInt(val);
+
+    // BASE CASES
+    if (val < 0) return "OOPS";
+    if (val < 1) return 0;
+    if (val == 1 ) return 1;
+
+    // ANYTHING PAST 2
+    let a = 0;
+    let b = 1;
+    let c = 0;
+    for (let i = 1; i < val; i++ ) {
+        c = a+b;
+        a = b;
+        b = c;
+    }
+
+    return c;
 };
 
 // Do not edit below this line
