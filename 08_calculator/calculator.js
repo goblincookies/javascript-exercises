8444151023f4675e43785fe227e4fd5184c3beab
goblincookies <goblincookiebox@gmail.com>
2024-11-08 12:57:56 -0500

    completes javascript exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return (a+b);
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return (a-b);
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+	return (a.reduce( (total, val) => {return total+val}, 0 ));
 };
 
-const multiply = function() {
+const multiply = function(a) {
+  return (a.reduce( (total, val) => {return total*val}, 1 ));
 
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return(a**b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let i = 0
+  return b = Array(a).fill(0)
+    .map( (val) => i+=1 )
+    .reduce( (total, val)=> {return total*val},1);
+
 };
 
 // Do not edit below this line
