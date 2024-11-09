800c9bef5285a7d79883e98701c07ac678b9caa8
goblincookies <goblincookiebox@gmail.com>
2024-11-08 20:02:45 -0500

    completes javascript exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,30 @@
-const findTheOldest = function() {
 
+const findTheOldest = function( people ) {
+
+    let oldest = {
+        name:"",
+        age:0,
+      }
+
+    let birth = 0;
+    let death = 0;
+    let age = 0;
+
+    people.forEach( person => {
+        death = 2024;
+        birth = 0;
+
+        if (Object.hasOwn( person, "yearOfDeath") ) death = person.yearOfDeath;
+        if (Object.hasOwn( person, "yearOfBirth") ) birth = person.yearOfBirth;
+        age = death - birth;
+
+        if (age > oldest.age) {
+            oldest.name = person.name;
+            oldest.age = age;
+        }
+
+    });
+    return oldest;
 };
 
 // Do not edit below this line
